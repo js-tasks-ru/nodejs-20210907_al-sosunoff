a4b74a75de7439a71066446670e65a9a7d2ac428
asosunoff <al.sosunoff@gmail.com>
2021-09-15 23:56:09 +0300

    fix summ

--- 01-intro/01-sum/sum.ts
+++ 01-intro/01-sum/sum.ts
@@ -1,11 +1,9 @@
 const isNumber = (x: number) => typeof x === "number" || typeof x === "bigint";
 
-function sum(a: number, b: number) {
+export default function sum(a: number, b: number) {
   if (!isNumber(a) || !isNumber(b)) {
     throw new TypeError("Not a number");
   }
 
   return a + b;
 }
-
-module.exports = sum;
