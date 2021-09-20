752f69e20345e6e91b52c1f237eb91d6b2a1aeb6
asosunoff <al.sosunoff@gmail.com>
2021-09-20 09:08:33 +0300

    fix file

--- 03-streams/05-file-server-delete/server.ts
+++ 03-streams/05-file-server-delete/server.ts
@@ -1,18 +1,18 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+import url, { URL } from 'url';
+import http from 'http';
+import path from 'path';
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+  const url = new URL(req.url ?? '', `http://${req.headers.host}`);
+
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
-
       break;
 
     default:
@@ -21,4 +21,4 @@ server.on('request', (req, res) => {
   }
 });
 
-module.exports = server;
+export default server;
