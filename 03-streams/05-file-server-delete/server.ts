d6aea8b5aa2a10366884cf29610330583030790e
asosunoff <al.sosunoff@gmail.com>
2021-09-20 09:54:51 +0300

    change to promise

--- 03-streams/05-file-server-delete/server.ts
+++ 03-streams/05-file-server-delete/server.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const url = new URL(req.url ?? '', `http://${req.headers.host}`);
 
   const pathname = url.pathname.slice(1);
@@ -20,20 +20,19 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE': {
-      // fs.Promises.rm
-      fs.unlink(filepath, (error) => {
-        if (!error) {
-          res.end('good');
+      try {
+        await fs.promises.rm(filepath);
+
+        res.end('good');
+      } catch (error: any) {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('file not found');
         } else {
-          if (error.code === 'ENOENT') {
-            res.statusCode = 404;
-            res.end('file not found');
-          } else {
-            res.statusCode = 500;
-            res.end();
-          }
+          res.statusCode = 500;
+          res.end();
         }
-      });
+      }
       break;
     }
     default:
