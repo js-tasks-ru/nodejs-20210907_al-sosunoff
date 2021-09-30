f063a8d056d430a773499df2b1edd5e886579020
asosunoff <al.sosunoff@gmail.com>
2021-09-30 20:10:12 +0300

    fox

--- 05-koajs/01-chat-app/controller.ts
+++ 05-koajs/01-chat-app/controller.ts
@@ -5,18 +5,21 @@ let clients: Array<ExtendableContext['res']> = [];
 
 export const subscribeGet: Parameters<typeof router.get>['2'] = async (ctx) => {
   clients.push(ctx.res);
+
   await new Promise(() => {});
 };
 
 export const publishPost: Parameters<typeof router.post>['2'] = async (ctx) => {
   const { message } = ctx.request.body;
 
-  clients.forEach((res) => {
-    res.statusCode = 200;
-    res.end(message);
-  });
+  if (message) {
+    clients.forEach((res) => {
+      res.statusCode = 200;
+      res.end(message);
+    });
 
-  clients = [];
+    clients = [];
+  }
 
   ctx.res.statusCode = 200;
 };
