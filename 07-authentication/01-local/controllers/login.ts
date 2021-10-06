82b67dab5e8b569d6dee063aeb673e05d610018a
asosunoff <al.sosunoff@gmail.com>
2021-10-06 22:17:24 +0300

    fix login

---
+++ 07-authentication/01-local/controllers/login.ts
@@ -0,0 +1,19 @@
+import { v4 as uuid } from 'uuid';
+import { passport } from '../libs/passport';
+import { router } from '../app';
+
+export const login: Parameters<typeof router.post>['2'] = async (ctx, next) => {
+  await passport.authenticate('local', async (err, user, info) => {
+    if (err) throw err;
+
+    if (!user) {
+      ctx.status = 400;
+      ctx.body = { error: info };
+      return;
+    }
+
+    const token = uuid();
+
+    ctx.body = { token };
+  })(ctx, next);
+};
