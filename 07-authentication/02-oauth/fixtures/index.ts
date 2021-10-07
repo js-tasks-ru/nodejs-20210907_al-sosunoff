b71a36b2552c13318d092b83d76a68d5bcf2ea0a
asosunoff <al.sosunoff@gmail.com>
2021-10-07 23:45:23 +0300

    fix

--- 07-authentication/02-oauth/fixtures/index.ts
+++ 07-authentication/02-oauth/fixtures/index.ts
@@ -1,16 +1,18 @@
-const User = require('../models/User');
-const mongoose = require('mongoose');
-const users = require('../../../data/users');
+import { User } from '../models/User';
+import { connection } from '../libs/connection';
+import users from '../../../data/users.json';
 
 (async () => {
   await User.deleteMany();
 
   for (const user of users.users) {
     const u = new User(user);
+    
     await u.setPassword(user.password);
+    
     await u.save();
   }
 
-  mongoose.disconnect();
+  connection.close();
   console.log(`All done, ${users.users.length} users have been saved in DB`);
 })();
