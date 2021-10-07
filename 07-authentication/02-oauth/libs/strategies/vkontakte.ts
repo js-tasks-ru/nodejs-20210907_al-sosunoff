86efb3b7fc12f58baa05fd06acdd74b6eaeea8fd
asosunoff <al.sosunoff@gmail.com>
2021-10-07 23:31:27 +0300

    fix vk

---
+++ 07-authentication/02-oauth/libs/strategies/vkontakte.ts
@@ -0,0 +1,21 @@
+import { Strategy as VkontakteStrategy } from 'passport-vkontakte';
+import { config } from '../../config';
+
+export const vkontakteStrategy = new VkontakteStrategy(
+  {
+    clientID: config.providers.vkontakte.app_id,
+    clientSecret: config.providers.vkontakte.app_secret,
+    callbackURL: config.providers.vkontakte.callback_uri,
+    apiVersion: '5.110',
+    // scope: ['user:email'],
+    // session: false,
+  },
+  function (accessToken, refreshToken, params, profile, done) {
+    done(
+      null,
+      false,
+      `функция аутентификации с помощью vkontakte не настроена`
+    );
+    /* authenticate('vkontakte', params.email, profile.displayName, done); */
+  }
+);
