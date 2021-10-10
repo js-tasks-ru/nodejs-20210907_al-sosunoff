d7fda2d3c8012d1471a67749905343d75b317b4e
asosunoff <al.sosunoff@gmail.com>
2021-10-10 23:57:10 +0300

    fix create token

--- 07-authentication/03-sessions/middleware/getTokenMiddleware.ts
+++ 07-authentication/03-sessions/middleware/getTokenMiddleware.ts
@@ -1,24 +1,38 @@
+import { UserDocument } from './../models/User/interfaces';
 import { v4 as uuid } from 'uuid';
 import { Session } from '../models/Session';
 import { app } from '../app';
+import { Error } from 'mongoose';
 
 export const getTokenMiddleware: Parameters<typeof app.use>['0'] = async (
   ctx,
   next
 ) => {
-  ctx.login = async (user) => {
-    const token = uuid();
-
-    const session = new Session({
-      token,
-      user: user._id,
-      lastVisit: new Date(),
-    });
-
-    await session.save();
-
-    return token;
-  };
+  ctx.getToken = (user: UserDocument) => new Promise<string>(async (res) => {
+    while (true) {
+      try {
+        const session = new Session({
+          token: uuid(),
+          user: user._id,
+          lastVisit: new Date(),
+        });
+  
+        await session.save();
+  
+        res(session.token);
+      } catch (err) {
+        if (err instanceof Error.ValidationError) {
+          if ('token' in err.errors && (err.errors.token as any).kind === 'unique') {
+            // repeat create session
+          } else {
+            throw err;
+          }
+        } else {
+          throw err;
+        }
+      }
+    }
+  });
 
   return next();
 };
