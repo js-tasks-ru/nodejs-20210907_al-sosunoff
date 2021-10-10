0f08abc92ea2e1b48ac15e74d4a40aa816a2e441
asosunoff <al.sosunoff@gmail.com>
2021-10-11 00:38:39 +0300

    fix app

--- 07-authentication/03-sessions/app.ts
+++ 07-authentication/03-sessions/app.ts
@@ -3,21 +3,21 @@ import Koa, { DefaultState, DefaultContext } from 'koa';
 import koaStatic from 'koa-static';
 import koaBodyparser from 'koa-bodyparser';
 import Router from 'koa-router';
-import { v4 as uuid } from 'uuid';
 
-import { Session } from './models/Session';
 import { handleMongooseValidationError } from './libs/validationErrors';
 import { mustBeAuthenticated } from './libs/mustBeAuthenticated';
 import { login } from './controllers/login';
 import { oauth, oauthCallback } from './controllers/oauth';
 import { me } from './controllers/me';
-import { UserDocument, UserSchema } from './models/User/interfaces';
+import { UserDocument } from './models/User/interfaces';
 import { catchErrorMiddleware } from './middleware/catchErrorMiddleware';
 import { indexMiddleware } from './middleware/indexMiddleware';
 import { getTokenMiddleware } from './middleware/getTokenMiddleware';
+import { mapUser } from './models/User/mapUser';
+import { whoAmIMiddleware } from './middleware/whoAmIMiddleware';
 
 interface Context extends DefaultContext {
-  user: UserSchema;
+  user: ReturnType<typeof mapUser>;
   getToken: (user: UserDocument) => Promise<string>;
 }
 
@@ -30,18 +30,10 @@ app.use(getTokenMiddleware);
 
 const router = new Router<any, Context>({ prefix: '/api' });
 
-router.use(async (ctx, next) => {
-  const header = ctx.request.get('Authorization');
-
-  if (!header) return next();
-
-  return next();
-});
-
 router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
-router.get('/me', me);
+router.get('/me', whoAmIMiddleware, me);
 
 app.use(router.routes());
 app.use(indexMiddleware);
