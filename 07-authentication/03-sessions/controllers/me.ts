b36faa436e937c03f6a8798425a339d0f069b085
asosunoff <al.sosunoff@gmail.com>
2021-10-11 01:08:18 +0300

    fix me

--- 07-authentication/03-sessions/controllers/me.ts
+++ 07-authentication/03-sessions/controllers/me.ts
@@ -1,9 +1,5 @@
 import { router } from '../app';
 
 export const me: Parameters<typeof router.get>['2'] = async (ctx, next) => {
-  if (ctx.user) {
-    ctx.body = ctx.user;
-  } else {
-    ctx.status = 401;
-  }
+  ctx.body = ctx.user;
 };
