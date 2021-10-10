87bd84b034d6b09658e6fed87abdd31696217ef9
asosunoff <al.sosunoff@gmail.com>
2021-10-11 00:39:24 +0300

    fix

--- 07-authentication/03-sessions/models/Session/interfaces.ts
+++ 07-authentication/03-sessions/models/Session/interfaces.ts
@@ -1,9 +1,10 @@
+import { UserDocument } from './../User/interfaces';
 import { Model, Document, Schema } from 'mongoose';
 
 export interface SessionSchema {
   token: string;
   lastVisit: Date;
-  user: Schema.Types.ObjectId;
+  user: Schema.Types.ObjectId | UserDocument;
 }
 
 export interface SessionDocument extends SessionSchema, Document {}
