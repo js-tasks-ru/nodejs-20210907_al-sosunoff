03e0f40eb137dbf2616ae4357ef3d182746ef1d5
asosunoff <al.sosunoff@gmail.com>
2021-10-11 17:34:41 +0300

    fix

--- 07-authentication/03-sessions/middleware/getSessionMiddleware.ts
+++ 07-authentication/03-sessions/middleware/getSessionMiddleware.ts
@@ -1,11 +1,32 @@
-import {router} from '../app';
-import { Session } from '../models/Session';;
+import jwt, {JwtPayload} from 'jsonwebtoken';
+import { Error } from 'mongoose';
+
+import { router } from '../app';
+import { secretOrPrivateKey } from '../constants';
 
 export const getSessionMiddleware: Parameters<typeof router.use>['1'] = async (
   ctx,
   next
 ) => {
-  const session = await Session.findOne({ token: ctx.token }).populate('user');
+  const user = jwt.decode(ctx.token);
+
+  try {
+    if(!jwt.verify(ctx.token, secretOrPrivateKey)) throw new Error("");
+
+    if(!user) throw new Error("");
+    
+  } catch (err) {
+    ctx.status = 401;
+  
+    ctx.body = {
+      error: 'Неверный аутентификационный токен',
+    };
+
+    return;
+  }
+  
+
+  /* const session = await Session.findOne({ token: ctx.token }).populate('user');
 
   if (!session) {
     ctx.status = 401;
@@ -15,9 +36,11 @@ export const getSessionMiddleware: Parameters<typeof router.use>['1'] = async (
     };
 
     return;
-  }
+  } */
 
-  ctx.sessionDocument = session;
+  ctx.user = {
+    email: (user as JwtPayload).email,
+    displayName: (user as JwtPayload).displayName };
 
   return next();
 };
