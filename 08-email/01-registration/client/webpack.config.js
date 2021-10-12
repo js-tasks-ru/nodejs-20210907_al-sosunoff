302ea71a0d2ca9cc19a58496b45d5b89f251ce60
asosunoff <al.sosunoff@gmail.com>
2021-10-12 23:30:39 +0300

    fix

--- 08-email/01-registration/client/webpack.config.js
+++ 08-email/01-registration/client/webpack.config.js
@@ -1,27 +1,68 @@
+/* eslint-disable */
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const fileName = (ext) => `[name].${ext}`;
+
+const CONFIGS = {
+  default: {
+    mode: 'development',
+
+    context: path.resolve(__dirname, `./src`),
+
+    devtool: 'eval-source-map',
+
+    entry: {
+      index: ['./index.tsx'],
+    },
+
+    output: {
+      filename: fileName('js'),
+      path: path.resolve(__dirname, '../public'),
+      publicPath: '/',
+    },
+
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.scss'],
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, './src/index.html'),
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [{ loader: 'babel-loader' }],
+        },
+
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: 'babel-loader' },
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: 'tsconfig.json',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+const config = CONFIGS[process.env.CONFIG] || CONFIGS.default;
 
 module.exports = {
-  mode: 'development',
-  entry: path.join(__dirname, 'src/index.js'),
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, '../public'),
-    publicPath: '/',
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
-    }),
-  ],
+  ...config,
 };
