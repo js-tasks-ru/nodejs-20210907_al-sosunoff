752b9dbc1923b3a1166de8594de2fc8774949395
asosunoff <al.sosunoff@gmail.com>
2021-10-12 23:31:02 +0300

    fi

--- 08-email/01-registration/client/src/components/Confirmation.tsx
+++ 08-email/01-registration/client/src/components/Confirmation.tsx
@@ -1,60 +1,76 @@
 import React, { Component } from 'react';
-import axios from "axios";
-import {Link} from "react-router-dom";
+import { axios } from '../axios';
+import { Link } from 'react-router-dom';
 
-export default class Confirmation extends Component {
-  constructor(props) {
+interface State {
+  success: boolean;
+  error: any;
+}
+
+type Props = any;
+
+export class Confirmation extends Component<Props, State> {
+  constructor(props: Props) {
     super(props);
-    
+
     this.state = {
-      success: null,
+      success: false,
       error: null,
     };
   }
-  
-  componentDidMount() {
+
+  async componentDidMount() {
     const token = this.props.match.params.token;
-    
-    axios.post('/api/confirm', {
-      verificationToken: token
-    }).then(() => {
+
+    try {
+      await axios.post('/confirm', {
+        verificationToken: token,
+      });
+
       this.setState({
         success: true,
       });
-    }).catch(error => {
+    } catch (error: any) {
       this.setState({
         error: error.response.data.error,
       });
-    });
+    }
   }
-  
+
   render() {
     const { success, error } = this.state;
-    
+
     if (success) {
       return (
         <div className="text-center border border-light p-5">
-          <p className="h4 mb-4">Поздравляем, ваш почтовый адрес успешно подтвержден!</p>
+          <p className="h4 mb-4">
+            Поздравляем, ваш почтовый адрес успешно подтвержден!
+          </p>
         </div>
       );
     }
-    
+
     if (error) {
       return (
         <div className="text-center border border-light p-5">
-          <p className="text-danger">При выполнени операции произошла ошибка.</p>
+          <p className="text-danger">
+            При выполнени операции произошла ошибка.
+          </p>
+          
           <p className="text-danger">{error}</p>
           <Link to="/">Регистрация</Link>
         </div>
       );
     }
-    
+
     return (
       <div className="row login-form justify-content-center align-items-center">
         <div className="col col-md-6">
           <div className="text-center border border-light p-5">
             <p className="h4 mb-4">Подтверждение почтового адреса</p>
+
             <p>Почтовый адрес подтверждается, пожалуйста, подождите.</p>
+            
             <div className="spinner-border" role="status">
               <span className="sr-only">Загрузка...</span>
             </div>
