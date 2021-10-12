5cc3b68627b12200dcf167ea0f84d7b825efbe06
asosunoff <al.sosunoff@gmail.com>
2021-10-12 23:49:00 +0300

    added

---
+++ 08-email/01-registration/models/User/index.ts
@@ -0,0 +1,55 @@
+import { UserDocument, UserModel, UserSchema } from './interfaces';
+import { Schema } from 'mongoose';
+import { connection } from '../../libs/connection';
+import { generatePassword, generateSalt } from './utils';
+
+const userSchema = new Schema<UserDocument, UserModel, UserSchema>({
+  email: {
+    type: String,
+    required: [true, 'E-mail пользователя не должен быть пустым.'],
+    validate: [
+      {
+        validator(value: string) {
+          return /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(value);
+        },
+        message: 'Некорректный email.',
+      },
+    ],
+    // unique: 'Такой email уже существует',
+    unique: true,
+  },
+  displayName: {
+    type: String,
+    required: [true, 'У пользователя должно быть имя'],
+    // unique: 'Такое имя уже существует',
+    unique: true,
+  },
+  verificationToken: {
+    type: String,
+    index: true,
+  },
+  passwordHash: {
+    type: String,
+  },
+  salt: {
+    type: String,
+  },
+}, {
+  timestamps: true,
+});
+
+userSchema.methods.setPassword = async function (password) {
+  this.salt = await generateSalt();
+
+  this.passwordHash = await generatePassword(this.salt, password);
+};
+
+userSchema.methods.checkPassword = async function (password) {
+  if (!password) return false;
+
+  const hash = await generatePassword(this.salt, password);
+
+  return hash === this.passwordHash;
+};
+
+export const User = connection.model<UserDocument, UserModel>('User', userSchema);
---
+++ 08-email/01-registration/models/User/interfaces.ts
@@ -0,0 +1,16 @@
+import { Model, Document } from 'mongoose';
+
+export interface UserSchema {
+  email: string;
+  displayName: string;
+  verificationToken: string;
+  passwordHash: string;
+  salt: string;
+}
+
+export interface UserDocument extends UserSchema, Document {
+  setPassword: (password: string) => Promise<void>;
+  checkPassword: (password: string) => Promise<boolean>;
+}
+
+export interface UserModel extends Model<UserDocument> {}
