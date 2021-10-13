06cb28a0c8af1e9a3f8686c19234b02114ed4b28
asosunoff <al.sosunoff@gmail.com>
2021-10-14 01:03:50 +0300

    fix

---
+++ 08-email/01-registration/controllers/products.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import { router } from '../app';
+import { Product } from '../models/Product';
+import { mapProduct } from '../mappers/product';
+
+export const productsBySubcategory: Parameters<typeof router.get>['2'] = async (
+  ctx,
+  next
+) => {
+  const { subcategory } = ctx.query;
+
+  if (!subcategory) return next();
+
+  const products = await Product.find({ subcategory: subcategory as any as mongoose.Schema.Types.ObjectId }).limit(20);
+
+  ctx.body = { products: products.map(mapProduct) };
+};
+
+export const productList: Parameters<typeof router.get>['2'] = async (
+  ctx,
+  next
+) => {
+  const products = await Product.find().limit(20);
+
+  ctx.body = { products: products.map(mapProduct) };
+};
+
+export const productById: Parameters<typeof router.get>['2'] = async (
+  ctx,
+  next
+) => {
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) {
+    ctx.throw(400, 'invalid product id');
+  }
+
+  const product = await Product.findById(ctx.params.id);
+
+  if (!product) {
+    return ctx.throw(404, `no product with ${ctx.params.id} id`);
+  }
+
+  ctx.body = { product: mapProduct(product) };
+};
