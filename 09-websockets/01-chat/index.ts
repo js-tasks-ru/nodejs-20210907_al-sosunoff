627f0a46fe4b31ac4004ae543a3258c1c0df7e79
asosunoff <al.sosunoff@gmail.com>
2021-10-18 22:26:54 +0300

    fix

--- 09-websockets/01-chat/index.ts
+++ 09-websockets/01-chat/index.ts
@@ -1,5 +1,5 @@
-const app = require('./app');
-const socket = require('./socket');
+import { app } from './app';
+import { socket } from './socket';
 
 const server = app.listen(3000, () => {
   console.log('App is running on http://localhost:3000');
